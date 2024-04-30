87106f96bed94c814009e43fbe6bf6c6d7e8a4d0
liang <lwy.work@outlook.com>
2024-04-30 23:46:36 +0800

    初始化并安装部分插件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+// vue组件
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 外部组件
+import '@/utils/vant-ui'
 
 Vue.config.productionTip = false
 
