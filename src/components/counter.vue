c121182f5299b1f9907a0c1d225297373c33cc6b
liang-Ryan <sky_tenshi@outlook.com>
2024-11-20 20:57:07 +0800

    添加库存判断

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="counter">
-    <button @click="toDetailPage('minus')">-</button>
+    <button :class="{ outOfStock: !Boolean(stock) || value === 1 }" @click="changeCount('minus', value === 1)">-</button>
     <input
       type="text"
       :value="value"
-      @change="changeCount"
+      @change="changeHandle"
     />
-    <button @click="toDetailPage('add')">+</button>
+    <button :class="{ outOfStock: !Boolean(stock) || value >= stock}" @click="changeCount('add', value >= stock)">+</button>
   </div>
 </template>
 
@@ -19,27 +19,35 @@ export default {
       type: Number,
       required: true,
       default: 1
+    },
+    stock: {
+      type: Number,
+      default: 0
     }
   },
 
   methods: {
-    toDetailPage (type) {
+    changeCount (type, stockCheck) {
+      if (!this.stock || stockCheck) return
+
       if (type === 'minus') {
         if (this.value > 1) this.$emit('input', this.value - 1)
-      } else if (type === 'add') {
+      }
+      if (type === 'add') {
         this.$emit('input', this.value + 1)
-      } else {
-        console.log(`传入的${type}有误`)
       }
     },
 
-    changeCount (e) {
+    changeHandle (e) {
       if (isNaN(+e.target.value)) {
         this.$toast('请输入数字')
         e.target.value = this.value
       } else if (e.target.value <= 0) {
         this.$toast('请输入大于0的数字')
         e.target.value = this.value
+      } else if (e.target.value > this.stock) {
+        this.$toast('已达到商品库存数量')
+        e.target.value = this.stock
       } else {
         this.$emit('input', +e.target.value)
       }
@@ -71,6 +79,10 @@ export default {
 
     display: flex;
     align-items: center;
+  }
+
+  .outOfStock {
+    cursor: not-allowed;
   }
 }
 </style>
