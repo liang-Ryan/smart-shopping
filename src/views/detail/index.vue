92b5e8190ac7a393f64a432e80bc511ae8643fca
liang-Ryan <sky_tenshi@outlook.com>
2024-11-20 20:59:15 +0800

    优化代码逻辑、格式、可读性

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="detail">
-    <van-nav-bar fixed title="商品详情页" left-arrow @click-left="$router.go(-1)" />
+    <van-nav-bar
+      fixed
+      title="商品详情页"
+      left-arrow
+      @click-left="$router.go(-1)"
+    />
 
     <!-- 轮播图 -->
-    <van-swipe :autoplay="3000" @change="onChange">
-      <van-swipe-item v-for="item in images" :key="item.file_id">
-        <img :src="item.external_url"/>
+    <van-swipe
+      :autoplay="3000"
+      @change="onChange"
+    >
+      <van-swipe-item
+        v-for="item in images"
+        :key="item.file_id"
+      >
+        <img :src="item.external_url" />
       </van-swipe-item>
 
       <template #indicator>
-        <div class="custom-indicator">{{ current }} / {{ images.length }}</div>
+        <div class="custom-indicator">{{ current }} / {{ images?.length }}</div>
       </template>
     </van-swipe>
 
@@ -24,21 +35,34 @@
       </div>
       <div class="msg text-ellipsis-2">{{ detail.goods_name }}</div>
 
-      <div class="service" @click="showServiceSheet = true">
+      <div
+        class="service"
+        @click="showServiceSheet = true"
+      >
         <div class="left-words">
-          <span v-for="item in serviceList" :key="item.service_id">
+          <span
+            v-for="item in serviceList"
+            :key="item.service_id"
+          >
             <van-icon name="passed" />{{ item.name }}
           </span>
         </div>
-        <div class="right-icon">
+        <div>
           <van-icon name="arrow" />
         </div>
       </div>
     </div>
 
     <!-- 商品保障弹出面板 -->
-    <van-action-sheet class="service-sheet" v-model="showServiceSheet" title="商品保障服务">
-      <div v-for="item in serviceList" :key="item.service_id">
+    <van-action-sheet
+      class="service-sheet"
+      v-model="showServiceSheet"
+      title="商品保障服务"
+    >
+      <div
+        v-for="item in serviceList"
+        :key="item.service_id"
+      >
         <van-icon name="passed" />{{ item.name }}
       </div>
     </van-action-sheet>
@@ -47,14 +71,32 @@
     <div class="comment">
       <div class="comment-title">
         <div class="left">商品评价 ({{ commentSum }}条)</div>
-        <div class="right" @click="showCommentList(-1, 1)">查看更多 <van-icon name="arrow" /> </div>
+        <div
+          class="right"
+          @click="showCommentList(-1, 1)"
+        >
+          查看更多 <van-icon name="arrow" />
+        </div>
       </div>
-      <div class="comment-list">
-        <div class="comment-item" v-for="item in firstThreeComment" :key="item.comment_id">
+      <div>
+        <div
+          class="comment-item"
+          v-for="item in topThreeComment"
+          :key="item.comment_id"
+        >
           <div class="top">
-            <img :src="item.user.avatar_url || defaultImg" alt="">
+            <img
+              :src="item.user.avatar_url || defaultAvatar"
+              alt=""
+            />
             <div class="name">{{ item.user.nick_name }}</div>
-            <van-rate :size="16" :value="item.score / 6" color="#ffd21e" void-icon="star" void-color="#eee"/>
+            <van-rate
+              :size="16"
+              :value="item.score / 6"
+              color="#ffd21e"
+              void-icon="star"
+              void-color="#eee"
+            />
           </div>
           <div class="content">{{ item.content }}</div>
           <div class="time">{{ item.create_time }}</div>
@@ -63,13 +105,30 @@
     </div>
 
     <!-- 商品评价弹出面板 -->
-    <van-action-sheet class="comment-sheet" v-model="showCommentSheet" title="商品评价">
-      <div class="comment-list">
-        <div class="comment-item" v-for="item in commentList" :key="item.comment_id">
+    <van-action-sheet
+      class="comment-sheet"
+      v-model="showCommentSheet"
+      title="商品评价"
+    >
+      <div>
+        <div
+          class="comment-item"
+          v-for="item in commentList"
+          :key="item.comment_id"
+        >
           <div class="top">
-            <img :src="item.user.avatar_url || defaultImg" alt="">
+            <img
+              :src="item.user.avatar_url || defaultAvatar"
+              alt=""
+            />
             <div class="name">{{ item.user.nick_name }}</div>
-            <van-rate :size="16" :value="item.score / 6" color="#ffd21e" void-icon="star" void-color="#eee"/>
+            <van-rate
+              :size="16"
+              :value="item.score / 6"
+              color="#ffd21e"
+              void-icon="star"
+              void-color="#eee"
+            />
           </div>
           <div class="content">{{ item.content }}</div>
           <div class="time">{{ item.create_time }}</div>
@@ -78,28 +137,56 @@
     </van-action-sheet>
 
     <!-- 商品描述 -->
-    <div class="content" v-html="detail.content"></div>
+    <div
+      class="content"
+      v-html="detail.content"
+    ></div>
 
     <!-- 底部 -->
     <div class="footer">
-      <div class="icon-home" @click="$router.push('/home')">
+      <div
+        class="icon-home"
+        @click="$router.push('/home')"
+      >
         <van-icon name="wap-home-o" />
         <span>首页</span>
       </div>
-      <div class="icon-cart" @click="$router.push('/cart')">
-        <van-icon name="shopping-cart-o" :badge="cartTotal > 0 ? cartTotal : ''"/>
+      <div
+        class="icon-cart"
+        @click="$router.push('/cart')"
+      >
+        <van-icon
+          name="shopping-cart-o"
+          :badge="cartTotal > 0 ? cartTotal : ''"
+        />
         <span>购物车</span>
       </div>
-      <div class="btn-add" @click="popSheet('cart')">加入购物车</div>
-      <div class="btn-buy" @click="popSheet('pay')">立刻购买</div>
+      <div
+        class="btn-add"
+        @click="popSheet('加入购物车')"
+      >
+        加入购物车
+      </div>
+      <div
+        class="btn-buy"
+        @click="popSheet('立刻购买')"
+      >
+        立刻购买
+      </div>
     </div>
 
     <!-- 购买弹出面板 -->
-    <van-action-sheet v-model="showPaySheet" :title="paySheetTitle">
+    <van-action-sheet
+      v-model="showPaySheet"
+      :title="paySheetTitle"
+    >
       <div class="action-sheet">
         <div class="sheet-content">
           <div class="left">
-            <img :src="detail.goods_image" alt="">
+            <img
+              :src="detail.goods_image"
+              alt=""
+            />
           </div>
           <div class="right">
             <div class="price">
@@ -114,11 +201,15 @@
         </div>
         <div class="num-box">
           <span>数量</span>
-          <counter v-model="count"></counter>
+          <counter v-model="count" :stock="detail.stock_total"></counter>
         </div>
         <div v-if="detail.stock_total > 0">
-          <div class="btn-cart" v-if="paySheetTitle === '加入购物车'" @click="order('cart')">加入购物车</div>
-          <div class="btn-pay" v-else  @click="order('pay')">立刻购买</div>
+          <div
+            :class="{ 'btn-cart': paySheetTitle === '加入购物车', 'btn-pay': paySheetTitle === '立刻购买'}"
+            @click="order(paySheetTitle)"
+          >
+            {{ paySheetTitle }}
+          </div>
         </div>
         <div class="btn-none" v-else>该商品已被抢完</div>
       </div>
@@ -127,9 +218,14 @@
 </template>
 
 <script>
-import defaultImg from '@/assets/default-avatar.png'
+import defaultAvatar from '@/assets/default-avatar.png'
 import counter from '@/components/counter.vue'
-import { goodsGetDetailAPI, goodsGetServiceAPI, goodsGetCommentsAPI, goodsGeCommentListAPI } from '@/api/goods-detail'
+import {
+  goodsGetDetailAPI,
+  goodsGetServiceAPI,
+  goodsGetCommentsAPI,
+  goodsGeCommentListAPI
+} from '@/api/goods-detail'
 import { cartGetSumAPI, cartPostAddAPI } from '@/api/cart'
 import { Dialog } from 'vant'
 
@@ -154,17 +250,15 @@ export default {
     return {
       // 商品详情
       detail: {},
-      images: [],
-      skuList: [],
 
       // 商品保障服务
       serviceList: [],
 
       // 商品评价
       commentSum: '',
-      firstThreeComment: [],
+      topThreeComment: [],
       commentList: [],
-      defaultImg,
+      defaultAvatar,
 
       // 轮播图
       current: 1,
@@ -176,16 +270,23 @@ export default {
       showServiceSheet: false,
       showCommentSheet: false,
       showPaySheet: false,
-      paySheetTitle: '加入购物车',
+      paySheetTitle: '',
 
       // 购物车商品总数
-      cartTotal: ''
+      cartTotal: 0
     }
   },
 
   computed: {
+    // 商品详情
     goods_id () {
       return this.$route.params.id
+    },
+    images () {
+      return this.detail.goods_images
+    },
+    skuList () {
+      return this.detail.skuList
     }
   },
 
@@ -194,8 +295,6 @@ export default {
     async getDetail () {
       const { data: { detail } } = await goodsGetDetailAPI(this.goods_id)
       this.detail = detail
-      this.images = detail.goods_images
-      this.skuList = detail.skuList
     },
 
     // 商品保障服务
@@ -207,7 +306,7 @@ export default {
     // 商品评价
     async getComment (amount) {
       const { data: { total, list } } = await goodsGetCommentsAPI(this.goods_id, amount)
-      this.firstThreeComment = list
+      this.topThreeComment = list
       this.commentSum = total
     },
     async showCommentList (type, page) {
@@ -218,68 +317,64 @@ export default {
 
     // 轮播图计数
     onChange () {
-      this.current >= 4 ? this.current = 1 : this.current++
+      this.current >= this.images.length ? this.current = 1 : this.current++
     },
 
     // 购物面板
-    popSheet (action) {
-      if (action === 'cart') {
-        this.paySheetTitle = '加入购物车'
-      } else if (action === 'pay') {
-        this.paySheetTitle = '立刻购买'
-      }
+    popSheet (type) {
+      this.paySheetTitle = type
       this.showPaySheet = true
     },
 
     // 登录验证
     isLogin () {
-      if (this.$store.getters.token) {
-        return true
-      } else {
-        Dialog.confirm({
-          message: '请登录后再进行操作',
-          confirmButtonText: '去登录',
-          cancelButtonText: '再逛逛'
+      if (this.$store.getters.token) return true
+
+      Dialog.confirm({
+        message: '请登录后再进行操作',
+        confirmButtonText: '去登录',
+        cancelButtonText: '再逛逛'
+      })
+        .then(() => {
+          this.$router.push({
+            path: '/login',
+            query: {
+              backUrl: this.$route.fullPath // 传递参数
+            }
+          })
         })
-          .then(() => {
-            this.$router.push({
-              path: '/login',
-              query: {
-                backUrl: this.$route.fullPath // 传递参数
-              }
-            })
-          })
-          .catch(() => {
-          })
-        return false
-      }
+        .catch(() => {})
+      return false
     },
 
     // 购买
     async order (type) {
-      if (this.isLogin()) { // 登录验证
-        if (type === 'cart') {
-        // 加入购物车
-          const { data, message } = await cartPostAddAPI(this.goods_id, this.count, this.skuList[0].goods_sku_id) // 后端仅提供1个sku_id
-          this.cartTotal = data.cartTotal
-          this.$toast(message)
-          this.showPaySheet = false
-        } else if (type === 'pay') {
-        // 立即购买
-          this.$router.push({
-            path: '/pay',
-            query: {
-              mode: 'buyNow',
-              obj: JSON.stringify({
-                goodsId: this.goods_id,
-                goodsNum: this.count,
-                goodsSkuId: this.skuList[0].goods_sku_id
-              })
-            }
-          })
-        } else {
-          console.log('type值错误')
-        }
+      if (!this.isLogin()) return // 登录验证
+
+      // 参数
+      const query = {
+        goodsId: this.goods_id,
+        goodsNum: this.count,
+        goodsSkuId: this.skuList[0].goods_sku_id
+      }
+
+      // 加入购物车
+      if (type === '加入购物车') {
+        const { data, message } = await cartPostAddAPI(...Object.values(query))
+        this.cartTotal = data.cartTotal
+        this.$toast(message)
+        this.showPaySheet = false
+      }
+
+      // 立即购买
+      if (type === '立刻购买') {
+        this.$router.push({
+          path: '/pay',
+          query: {
+            mode: 'buyNow',
+            obj: JSON.stringify(query)
+          }
+        })
       }
     },
 
@@ -299,6 +394,7 @@ export default {
   img {
     display: block;
     width: 100%;
+    -webkit-user-drag: none;
   }
 
   // 轮播图计数
@@ -306,9 +402,11 @@ export default {
     position: absolute;
     right: 10px;
     bottom: 10px;
-    padding: 5px 10px;
+
     border-radius: 15px;
+    padding: 5px 10px;
     background: rgba(0, 0, 0, 0.1);
+
     font-size: 12px;
   }
 
@@ -326,6 +424,7 @@ export default {
 
       .oldprice {
         margin-left: 5px;
+
         color: #959595;
         font-size: 16px;
         text-decoration: line-through;
@@ -334,6 +433,7 @@ export default {
       .sellcount {
         position: relative;
         top: 4px;
+
         color: #959595;
         font-size: 16px;
       }
@@ -341,6 +441,7 @@ export default {
 
     .msg {
       margin-top: 5px;
+
       font-size: 16px;
       line-height: 24px;
     }
@@ -348,10 +449,12 @@ export default {
     .service {
       margin-top: 10px;
       background-color: #fafafa;
-      display: flex;
-      justify-content: space-between;
+
       font-size: 16px;
       line-height: 40px;
+
+      display: flex;
+      justify-content: space-between;
 
       .left-words {
         span {
@@ -359,6 +462,7 @@ export default {
         }
         .van-icon {
           margin-right: 4px;
+
           color: #fa2209;
         }
       }
@@ -367,13 +471,15 @@ export default {
 
   .service-sheet {
     padding: 0 10px;
+
     font-size: 16px;
     line-height: 40px;
 
     .van-icon {
-          margin-right: 4px;
-          color: #fa2209;
-        }
+      margin-right: 4px;
+
+      color: #fa2209;
+    }
   }
 
   .comment {
@@ -388,37 +494,42 @@ export default {
       }
     }
   }
+
   .comment-sheet {
     padding: 0 10px;
   }
+
   .comment-item {
-      line-height: 30px;
-      font-size: 16px;
+    font-size: 16px;
+    line-height: 30px;
 
-      .top {
-        margin-top: 20px;
-        height: 30px;
-        display: flex;
-        align-items: center;
+    .top {
+      margin-top: 20px;
+      height: 30px;
 
-        ::v-deep img {
-          width: 20px;
-          height: 20px;
-        }
+      display: flex;
+      align-items: center;
 
-        .name {
-          margin: 0 10px;
-        }
+      ::v-deep img {
+        width: 20px;
+        height: 20px;
       }
 
-      .time {
-        color: #999;
+      .name {
+        margin: 0 10px;
       }
     }
 
+    .time {
+      color: #999;
+    }
+  }
+
   .content {
     width: 100%;
-    overflow: scroll;
+
+    word-break: break-all;
+
     ::v-deep img {
       width: 100% !important;
     }
@@ -428,21 +539,24 @@ export default {
     position: fixed;
     left: 0;
     bottom: 0;
+
     border-top: 1px solid #ccc;
     width: 100%;
     height: 55px;
     background-color: #fff;
+
     display: flex;
     justify-content: space-evenly;
     align-items: center;
 
     .icon-home,
     .icon-cart {
+      font-size: 14px;
+
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      font-size: 14px;
 
       .van-icon {
         font-size: 24px;
@@ -451,14 +565,15 @@ export default {
 
     .btn-add,
     .btn-buy {
+      border-radius: 18px;
       width: 120px;
       height: 36px;
-      line-height: 36px;
       background-color: #ffa900;
-      border-radius: 18px;
-      text-align: center;
+
       color: #fff;
       font-size: 14px;
+      line-height: 36px;
+      text-align: center;
     }
 
     .btn-buy {
@@ -481,13 +596,16 @@ export default {
 
       .right {
         flex: 1;
+
         padding: 10px;
 
         .price {
           color: #fe560a;
           font-size: 24px;
+
           .price-icon {
-            margin-right:5px;
+            margin-right: 5px;
+
             font-size: 14px;
           }
         }
@@ -496,23 +614,29 @@ export default {
 
     .num-box {
       padding: 10px;
+
       display: flex;
       justify-content: space-between;
       align-items: center;
     }
 
-    .btn-cart, .btn-pay, .btn-none {
-      height: 40px;
-      line-height: 40px;
-      background-color: rgb(255, 148, 2);
+    .btn-cart,
+    .btn-pay,
+    .btn-none {
       margin: 20px;
       border-radius: 20px;
+      height: 40px;
+      background-color: rgb(255, 148, 2);
+
       color: rgb(255, 255, 255);
+      line-height: 40px;
       text-align: center;
     }
+
     .btn-pay {
       background-color: #fe5630;
     }
+
     .btn-none {
       background-color: #cccccc;
     }
